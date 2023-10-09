b27377e4ca1091cd45df94f6c22547f1371283bd
Deividas Gailius <deividasgailius86@gmail.com>
2023-10-09 21:13:41 +0300

    adding lesson1

---
+++ lesson1/main.py
@@ -0,0 +1,18 @@
+# print("Labas Antanai")
+# my_name="Antanas"
+# my_age=26
+# # print(my_name+str(my_age) )
+# first_number = 22
+# second_number = 21
+
+# # sum
+# print("two number sum:", first_number + second_number)
+
+# # sum
+# print("two number sharing:", first_number / second_number)
+# print("two number multiplication:", first_number * second_number)
+# print("two number substraction", first_number - second_number)
+# print("two number advancement degree", first_number ** second_number)
+
+my_name = input("enter my name:")
+print("my name is:", my_name)
